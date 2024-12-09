f607ff1ff93e82ff87b2ec399618b14a54480e13
Malek Ashraf <111132898+MalekAshraf@users.noreply.github.com>
2024-12-09 14:37:03 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from "react";
+// index.js
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App"; 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+
+// Render the application
+const root = document.getElementById("root");
+
+createRoot(root).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
